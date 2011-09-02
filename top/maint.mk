74524a2c993a809bbc20dcc33e1c09789c2a909c
Martin von Gagern <Martin.vGagern@gmx.net>
2011-09-02 21:24:31 +0200

    Ignore missing files from gl_other_headers_. Avoids some error messages.

--- top/maint.mk
+++ top/maint.mk
@@ -683,6 +683,7 @@ gl_extract_significant_defines_ = \
 	@gen_h=$(gl_generated_headers_);				\
 	(cd $(gnulib_dir)/lib;						\
 	  for f in *.in.h $(gl_other_headers_); do			\
+	    test -e $$f &&						\
 	    perl -lne '$(gl_extract_significant_defines_)' $$f;		\
 	  done;								\
 	) | sort -u							\
